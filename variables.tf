2b2d30b7b8124ef2f7dee6d2d67337e28ecda1b9
Anusuya-murugiah <133192318+Anusuya-murugiah@users.noreply.github.com>
2024-11-28 20:17:51 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,18 @@ varaiable "vpc_name" {
   type = string
   description = "dev project1 in eu-west-1"
 }
+
+variable "cidr_block" {
+   type = string
+   description = "dev project1 in eu-west-1"
+}
+
+variable "public_cidr_block"  {
+  type = list(string)
+  description = "public cidr block for dev pro1"
+}
+
+variable "eu_availability_zone"  {
+  type = list(string)
+  description = "avaiability zone of dev pro1"
+}
