25e15fdc2f46f88dc82828395691dcf7d221b136
Anusuya-murugiah <133192318+Anusuya-murugiah@users.noreply.github.com>
2024-11-28 20:52:51 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,12 +8,21 @@ variable "cidr_block" {
    description = "dev project1 in eu-west-1"
 }
 
-variable "public_cidr_block"  {
+variable "cidr_public_subnet"  {
   type = list(string)
-  description = "public cidr block for dev pro1"
+  description = "cidr public subnet for dev pro1"
+}
+
+variable "cidr_private_subnet" {
+  type = list(string)
+  description = "cidr private subnet for dev pro1"
 }
 
 variable "eu_availability_zone"  {
   type = list(string)
   description = "avaiability zone of dev pro1"
 }
+
+
+
+
