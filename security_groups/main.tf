e7c2219d568cd6ea876ef4c5ab3b59a3dc26a3ca
Anusuya-murugiah <133192318+Anusuya-murugiah@users.noreply.github.com>
2024-12-03 20:11:59 +0530

    Update main.tf

--- security_groups/main.tf
+++ security_groups/main.tf
@@ -1,7 +1,9 @@
+variable "vpc_id" {}
+
 resource "aws_resource_group" {
   name = var.security_group
   description = "enable port for SSH(22) and HTTP(80)
-  vpc_id = 
+  vpc_id = var.vpc_id
 
 }
 
