edb32650a75ba26eec201fbd5fb5775e797d9006
Anusuya-murugiah <133192318+Anusuya-murugiah@users.noreply.github.com>
2024-12-04 19:52:51 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -23,6 +23,25 @@ variable "eu_availability_zone"  {
   description = "avaiability zone of dev pro1"
 }
 
+variable ami_id {}
+variable instance_type {}
+variable tag_name {}
+variable subnet_id {}
+variable vpc_security_group_ids {}
+variable associate_public_key_id {}
+variable user_data {}
+variable public_key {}
+
+variable "ami_id" {
+  type = string
+  description = "amazon machine image"
+}
+
+variable "public_key" {
+  type = "string"
+  description = "local host public key"
+}
+
 
 
 
