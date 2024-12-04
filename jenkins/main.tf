8d9e3dc4793c721e7f0aa31467c8017a427b445b
Anusuya-murugiah <133192318+Anusuya-murugiah@users.noreply.github.com>
2024-12-04 19:31:07 +0530

    Update main.tf

--- jenkins/main.tf
+++ jenkins/main.tf
@@ -1,10 +1,38 @@
+variable ami_id {}
+variable instance_type {}
+variable tag_name {}
+variable subnet_id {}
+variable vpc_security_group_ids {}
+variable associate_public_key_id {}
+variable user_data {}
+variable public_key {}
 
-
-
-resource "aws_ec2_instance" "local-host" {
-  ami = "
+resource "aws_instance" "local-host" {
+  ami = var.ami_id
   instance_type = var.instance_type
-  availability_zone = var.availability_zone
-  public_key = 
+  tags = {
+    name = var.tag_name
+  }
+  key_name = "aws_ec2_instance"
+  subnet_id = var.subnet_id
+  vpc_security_group_ids = var.vpc_security_group_ids
+  associate_public_key_id = var.associate_public_key_id
+
+  user_data = var.user_data
+
+  metadata_option {
+    http_endpoint = "enabled"
+    http_tokens = "required"
+  }
+}
+
+resource "aws_key_pair" "local_host_public_key" {
+  key_name = "aws_ec2_instance"
+  public_key = var.public_key
+}
+
+
+  
+
   
   
