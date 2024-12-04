4f2f3fe283afacadae8649ae41c8e6cc965ee654
Anusuya-murugiah <133192318+Anusuya-murugiah@users.noreply.github.com>
2024-12-04 19:04:56 +0530

    Update main.tf

--- jenkins/main.tf
+++ jenkins/main.tf
@@ -1,5 +1,10 @@
 
 
 
-resource "aws_ec2_instance" "instance" {
+resource "aws_ec2_instance" "local-host" {
+  ami = "
+  instance_type = var.instance_type
+  availability_zone = var.availability_zone
+  public_key = 
+  
   
