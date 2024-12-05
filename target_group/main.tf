9bb08d8be175539f7df71d461192fbc5ec81a34f
Anusuya-murugiah <133192318+Anusuya-murugiah@users.noreply.github.com>
2024-12-05 13:39:37 +0530

    Update main.tf

--- target_group/main.tf
+++ target_group/main.tf
@@ -11,17 +11,26 @@ output "tg_arn" {
 
 resource "aws_lb_target_group" "tg" {
   name = var.tg_name
-  target_type = var.target_type
   tg_protocol = var.tg_protocol
   tg_port = var.tg_port
   tg_vpc_id = var.tg_vpc_id
+
+  health_check {
+    path = "/login"
+    port = "8080"
+    healthy_threshold = 6
+    unhealthy_threshold = 2
+    timeout = 2
+    interval = 5
+    matcher = "200"
+  }
   
 }  
 
 resource "aws_lb_target_group_attachment" "lga" {
   target_group_arn = tg_arn
-  tg_instance = var.tg_instance
-  port = var.tg_port
+  target_id = var.target_id
+  port = 8080
 }
   
   
